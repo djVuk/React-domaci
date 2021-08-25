3f2d7266dcfa263688e65fc4a9570e85ca2973ca
VUK DJORDJEVIC <djordjevicvuk123@gmail.com>
2021-08-25 20:04:06 +0200

    blogpostKomponenta

---
+++ vukdomaci/src/post/Post.jsx
@@ -0,0 +1,23 @@
+import "./post.css"
+
+export default function Post() {
+    return (
+        <div className="post">
+            <img className="postImg" src="https://www.magazinauto.com/wp-content/uploads/2020/06/IMG_4003-680x365.jpg" alt="" />
+            <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">Music</span>
+                    <span className="postCat">Life</span>
+                </div>
+                <span className="postTitle">
+                    Lorem ipsum.
+                </span>
+                <hr />
+                <span className="postDate">1 hour ago</span>
+            </div>
+            <p className="postDesc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptate pariatur autem temporibus, debitis sed placeat fuga iste maxime officia quaerat culpa quisquam recusandae reprehenderit nisi iusto! Natus, reiciendis. Incidunt.
+            Dolorum eum ipsam ipsa, rem aperiam nulla temporibus voluptas nihil ad cumque aliquid corrupti minima dignissimos molestiae vel inventore suscipit pariatur sunt! Distinctio, sit quaerat! Sed praesentium placeat rerum modi?
+            Est eligendi aspernatur ducimus esse, repudiandae, reprehenderit voluptates aut, illo quisquam ratione facilis laudantium architecto voluptate facere! Explicabo, ipsa incidunt ex atque quas enim dignissimos alias asperiores, laudantium laboriosam eaque?</p>
+        </div>
+    )
+}
