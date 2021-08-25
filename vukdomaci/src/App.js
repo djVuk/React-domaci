b95ce5e51350f48e3658b01972076228ed4ea850
VUK DJORDJEVIC <djordjevicvuk123@gmail.com>
2021-08-25 21:41:11 +0200

    newPostKOmponenta

--- vukdomaci/src/App.js
+++ vukdomaci/src/App.js
@@ -1,12 +1,15 @@
+import Home from "./pages/home/Home"
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Posts from "./components/posts/Posts";
 
 function App() {
   return (
     <>
       <Topbar/>
       
-      <Single/>
+      <Write/>
       
     </>
   );
